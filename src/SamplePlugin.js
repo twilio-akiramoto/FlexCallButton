0c63629cd998908afea2a335a732ab15ee59cf3f
akiramoto <akiramoto@twilio.com>
2022-11-14 18:48:27 -0600

    First test
    1st Commit

---
+++ src/SamplePlugin.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FlexPlugin } from "@twilio/flex-plugin";
+
+//import CustomTaskList from './components/CustomTaskList/CustomTaskList';
+import addCallButton from "./addCallButton";
+
+const PLUGIN_NAME = "SamplePlugin";
+
+export default class SamplePlugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * This code is run when your plugin is being started
+   * Use this to modify any UI components or attach to the actions framework
+   *
+   * @param flex { typeof import('@twilio/flex-ui') }
+   */
+  async init(flex, manager) {
+    addCallButton(flex, manager);
+  }
+}
